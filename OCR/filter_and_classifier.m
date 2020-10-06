c378062ed92efe32790d84ad3bb6d8da3d2775c7
Renato Matos <renatomiguelmatos@gmail.com>
2020-10-06 23:22:36 +0100

    Update filter_and_classifier.m

--- OCR/filter_and_classifier.m
+++ OCR/filter_and_classifier.m
@@ -1,17 +1,16 @@
 %DATASET
-P = load('numbers.mat');
+load('numbers.mat');
+P = numbers;
 
 %TARGET
-t1 = [1 0 0 0 0 0 0 0 0 0]'
-t2 = [0 1 0 0 0 0 0 0 0 0]'
-t3 = [0 0 1 0 0 0 0 0 0 0]'
-t4 = [0 0 0 1 0 0 0 0 0 0]'
-t5 = [0 0 0 0 1 0 0 0 0 0]'
-t6 = [0 0 0 0 0 1 0 0 0 0]'
-t7 = [0 0 0 0 0 0 1 0 0 0]'
-t8 = [0 0 0 0 0 0 0 1 0 0]'
-t9 = [0 0 0 0 0 0 0 0 1 0]'
-t0 = [0 0 0 0 0 0 0 0 0 1]'
+load('PerfectArial.mat');
+
+T = Perfect;
+
+for i = 1:49
+     T = [T Perfect];
+ end
+
 
 %CREATE ASSOCIATIVE MEMORY AS FILTER
 %net = network
@@ -20,5 +19,34 @@ t0 = [0 0 0 0 0 0 0 0 0 1]'
 
 %CREATE BINARY PERCEPTRON AS FILTER
 filterB = perceptron;
+filterB = configure(filterB,P,T);
 filterB.trainFcn = 'trainc';
-filterB.adapFcn = 'learnp';
+filterB.adaptFcn = 'learnp';
+
+view(filterB)
+
+filterB = train(filterB,P,T);
+
+test = sim(filterB,P(:,9));
+
+showim(test)
+
+
+%PARA QUANDO SE FIZER O CLASSIFIER
+%TARGET
+% t1 = [1 0 0 0 0 0 0 0 0 0]';
+% t2 = [0 1 0 0 0 0 0 0 0 0]';
+% t3 = [0 0 1 0 0 0 0 0 0 0]';
+% t4 = [0 0 0 1 0 0 0 0 0 0]';
+% t5 = [0 0 0 0 1 0 0 0 0 0]';
+% t6 = [0 0 0 0 0 1 0 0 0 0]';
+% t7 = [0 0 0 0 0 0 1 0 0 0]';
+% t8 = [0 0 0 0 0 0 0 1 0 0]';
+% t9 = [0 0 0 0 0 0 0 0 1 0]';
+% t0 = [0 0 0 0 0 0 0 0 0 1]';
+% 
+% T = [t1 t2 t3 t4 t5 t6 t7 t8 t9 t0];
+% 
+% for i = 1:49
+%     T = [T t1 t2 t3 t4 t5 t6 t7 t8 t9 t0];
+% end
