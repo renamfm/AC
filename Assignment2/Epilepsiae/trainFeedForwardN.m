4a65c12b3291d1086a23feec623db0a0d0c3781a
Sérgio <sergiomanuelmachado@hotmail.com>
2020-11-09 00:55:15 +0000

    Minor change

--- Assignment2/Epilepsiae/trainFeedForwardN.m
+++ Assignment2/Epilepsiae/trainFeedForwardN.m
@@ -1,13 +1,48 @@
-function net = trainFeedForwardN(X, T, hiddenSizes, trainFcn)
+function  [network] = trainFeedForwardN(dataB,trainF, neuronsN, hLayersN, transferF,errorsOn)
+    
+    target = createTarget(dataB);
+         
+    hiddenLayers = zeros(1, hLayersN);
+    hiddenLayers(1,:) = neuronsN;
+    %Create feedfoward nn
+    net = feedforwardnet(hiddenLayers,trainF);
+  
+    %Transfer function
+    for i = 1: hLayersN
+        net.layers{i}.transferFcn = transferF;
+    end
+    
+   %Train parameters
+    net.trainParam.lr = 0.01;
+    net.performFcn = 'msereg';
+    %net.performParam.regularization = 0.9;
 
-    %generate the feedforward network
-    net = feedforwardnet(hiddenSizes, trainFcn);
+    %Stopping Criteria
+    net.trainParam.min_grad = 1e-9;
+    net.trainParam.max_fail = 100;
+    net.trainParam.goal = 1e-9;
+    net.trainParam.epochs = 1000;
     
-    %train net with X and T
-    net = train(net, x, t);
+    %Divisão do Set Balanceado para treino e validação
+    net.divideFcn = 'divideblock';
+    net.divideParam.trainRatio = 0.85;
+    net.divideParam.valRatio = 0.15;
     
-    view(net)
+    [~,C] = size(target);
+    interIctalL = nnz(all(target==[1 0 0]'));
+    preIctalL = nnz(all(target==[0 1 0]'));
+    ictalL = nnz(all(target==[0 0 1]'));
     
+    %Calculate de error weight matrix
+    EW = all(target==[1 0 0]')*(C/interIctalL) + all(target==[0 1 0]')*(C/preIctalL) + all(target==[0 0 1]')*(C/ictalL);    
     
+    %view(net)
+    %FIX: Use Parallel Computing
+    if(errorsOn == 1)
+        network = train(net,dataB.FeatVectSel,target,[],[],EW);
+    else
+        network = train(net,dataB.FeatVectSel,target);
+    end
+    view(net)  
 end
 
