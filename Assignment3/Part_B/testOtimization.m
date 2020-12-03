bc55edb4de39dcc841f71abc5be596a3f0b3f655
Sérgio <sergiomanuelmachado@hotmail.com>
2020-12-03 21:39:46 +0000

    corrected mse

--- Assignment3/Part_B/testOtimization.m
+++ Assignment3/Part_B/testOtimization.m
@@ -1,30 +1,31 @@
 function [bGridRes, bSubRes, bFcmRes, hGridRes, hSubRes, hFcmRes] = testOtimization(bGrid, bSub, bFcm,hGrid, hSub, hFcm, testData)
     input = testData(:,1:6); %The 7th column is the output
     output = testData(:,7);
+    L = length(output);
     
     %__________________________________________________
     %Backpropagation GridPartition
     bGridRes.output = evalfis(bGrid, input);
-    bGridRes.mse = immse(bGridRes.output, output);
+    bGridRes.mse = sum((output-bGridRes.output).^2)/L;
     
     %Backpropagation SubtractiveClustering
     bSubRes.output = evalfis(bSub, input);
-    bSubRes.mse = immse(bSubRes.output, output);
+    bSubRes.mse = sum((output-bSubRes.output).^2)/L;
     
     %Backpropagation FCMClustering
     bFcmRes.output = evalfis(bFcm, input);
-    bFcmRes.mse = immse(bFcmRes.output, output);
+    bFcmRes.mse = sum((output-bFcmRes.output).^2)/L;
    
     %__________________________________________________
     %Hybrid GridPartition
     hGridRes.output = evalfis(hGrid, input);
-    hGridRes.mse = immse(hGridRes.output, output);
+    hGridRes.mse = sum((output-hGridRes.output).^2)/L;
     
     %Hybrid SubtractiveClustering
     hSubRes.output = evalfis(hSub, input);
-    hSubRes.mse = immse(hSubRes.output, output);
+    hSubRes.mse = sum((output-hSubRes.output).^2)/L;
     
     %Hybrid FCMClustering
     hFcmRes.output = evalfis(hFcm, input);
-    hFcmRes.mse = immse(hFcmRes.output, output);
+    hFcmRes.mse = sum((output-hFcmRes.output).^2)/L;
 end
