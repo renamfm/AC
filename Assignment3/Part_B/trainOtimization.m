4e40071d9e4506ac285d7c1cb88ccc4eddf776b6
Sérgio <sergiomanuelmachado@hotmail.com>
2020-12-05 16:23:45 +0000

    saved fis

--- Assignment3/Part_B/trainOtimization.m
+++ Assignment3/Part_B/trainOtimization.m
@@ -10,5 +10,17 @@ function [trainedGrid, trainedSub, trainedFcm] = trainOtimization(grid, sub, fcm
    
    %FCM
    options = anfisOptions('OptimizationMethod', type,'InitialFIS',fcm);
-   trainedFcm = anfis(trainData, options);  
+   trainedFcm = anfis(trainData, options);
+   
+   if(type == 0)
+       name = 'backprop';
+   else
+       if(type == 1)
+           name = 'hybrid';
+       end
+   end
+   
+   writeFIS(trainedGrid, strcat(name,'_grid'));
+   writeFIS(trainedSub, strcat(name,'_sub'));
+   writeFIS(trainedFcm, strcat(name,'_fcm'));  
 end
